38fe231078b9cbcccb7e5762bad6a0fea3e7f83f
kongxiangwen <xw.kong@hnair.com>
2019-02-20 10:59:24 +0800

    Modify ret code and ret msg retrive type.

--- brave-dubbo/src/main/java/com/dcits/brave/dubbo/DubboClientResponseAdapter.java
+++ brave-dubbo/src/main/java/com/dcits/brave/dubbo/DubboClientResponseAdapter.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by chenjg on 16/7/24.
@@ -22,7 +24,8 @@ public class DubboClientResponseAdapter implements ClientResponseAdapter {
     private Invocation invocation;
 
     private Exception exception;
-
+    private static  Pattern pattern_ret = Pattern.compile("\"retCode\"\\s*:\\s*\"(.*?)\\s*\",.*retMsg\"\\s*:\\s*\"(.*?)\\s*\"");
+    private static  Pattern pattern_status = Pattern.compile("\"retStatus\"\\s*:\\s*\"(.*?)\\s*\"");
     public DubboClientResponseAdapter(Exception exception) {
         this.exception = exception;
     }
@@ -46,6 +49,8 @@ public class DubboClientResponseAdapter implements ClientResponseAdapter {
             if(rpcResult.hasException()){
                 KeyValueAnnotation keyValueAnnotation=  KeyValueAnnotation.create("exception",rpcResult.getException().getMessage());
                 annotations.add(keyValueAnnotation);
+                KeyValueAnnotation kStatus=  KeyValueAnnotation.create("status","failed");
+                annotations.add(kStatus);
             }else{
                 KeyValueAnnotation keyValueAnnotation=  KeyValueAnnotation.create("status","success");
                 annotations.add(keyValueAnnotation);
@@ -57,30 +62,64 @@ public class DubboClientResponseAdapter implements ClientResponseAdapter {
             if(jsonStr != null){
                 KeyValueAnnotation keyValueAnnotation=  KeyValueAnnotation.create("RESPONSE_INFO",jsonStr);
                 annotations.add(keyValueAnnotation);
-                Map map = BraveTracerFilter.getObjectMap(rpcResult.getResult());
-                if(map.get("retJsonObject") != null ){
+                /*Map<String,Object> map = BraveTracerFilter.getObjectMap(rpcResult.getResult());
+                if(map.get("rs") != null ){
 
-                    String code = (String)((Map)(((JSONArray)map.get("retJsonObject")).get(0))).get("retCode");
-                    KeyValueAnnotation kCode=  KeyValueAnnotation.create("RETCODE",code);
+                    String code = (String)((Map)(((JSONArray)((Map)map.get("rs")).get("ret")).get(0))).get("retCode");
+                    KeyValueAnnotation kCode=  KeyValueAnnotation.create("RET_CODE",code);
                     annotations.add(kCode);
 
-                    String msg = (String)((Map)(((JSONArray)map.get("retJsonObject")).get(0))).get("retMsg");
-                    KeyValueAnnotation kMsg=  KeyValueAnnotation.create("RETMSG",msg);
+                    String msg = (String)((Map)(((JSONArray)((Map)map.get("rs")).get("ret")).get(0))).get("retMsg");
+                    KeyValueAnnotation kMsg=  KeyValueAnnotation.create("RET_MSG",msg);
                     annotations.add(kMsg);
                 }
+                if(map.get("retStatus") != null){
+                    String retStatus = (String)map.get("retStatus");
+                    KeyValueAnnotation kStatus=  KeyValueAnnotation.create("RET_STATUS",retStatus);
+                    annotations.add(kStatus);
+
+                }*/
+
+                Matcher matcher = pattern_ret.matcher(jsonStr);
+                if(matcher.find() && matcher.groupCount() > 1) {
+                    KeyValueAnnotation kCode=  KeyValueAnnotation.create("RET_CODE",matcher.group(1));
+                    annotations.add(kCode);
+                    KeyValueAnnotation kMsg=  KeyValueAnnotation.create("RET_MSG",matcher.group(2));
+                    annotations.add(kMsg);
+                }
+                matcher = pattern_status.matcher(jsonStr);
+                if(matcher.find()) {
+                    KeyValueAnnotation kStatus=  KeyValueAnnotation.create("RET_STATUS",matcher.group(1));
+                    annotations.add(kStatus);
+                }
+
+
+
             }
         }else if(invocation.getMethodName() != null &&  invocation.getMethodName().equals("$invoke")){
             String jsonStr = BraveTracerFilter.getObjectMapStr(rpcResult.getResult());
             if(jsonStr != null){
                 KeyValueAnnotation keyValueAnnotation=  KeyValueAnnotation.create("RESPONSE_INFO",jsonStr);
                 annotations.add(keyValueAnnotation);
-                Map map = (Map)rpcResult.getResult();
+                /*Map map = (Map)rpcResult.getResult();
                 if(map.get("retJsonObject") != null ){
                     KeyValueAnnotation kCode=  KeyValueAnnotation.create("RETCODE",(String)((Map)map.get("retJsonObject")).get("retCode"));
                     annotations.add(kCode);
 
                     KeyValueAnnotation kMsg=  KeyValueAnnotation.create("RETMSG",(String)((Map)map.get("retJsonObject")).get("retMsg"));
                     annotations.add(kCode);
+                }*/
+                Matcher matcher = pattern_ret.matcher(jsonStr);
+                if(matcher.find() && matcher.groupCount() > 1) {
+                    KeyValueAnnotation kCode=  KeyValueAnnotation.create("RET_CODE",matcher.group(1));
+                    annotations.add(kCode);
+                    KeyValueAnnotation kMsg=  KeyValueAnnotation.create("RET_MSG",matcher.group(2));
+                    annotations.add(kMsg);
+                }
+                matcher = pattern_status.matcher(jsonStr);
+                if(matcher.find()) {
+                    KeyValueAnnotation kStatus=  KeyValueAnnotation.create("RET_STATUS",matcher.group(1));
+                    annotations.add(kStatus);
                 }
             }
         }
