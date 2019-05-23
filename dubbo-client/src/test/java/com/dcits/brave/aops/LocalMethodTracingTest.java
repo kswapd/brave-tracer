ebc066aab45048876c5bf9bc63373641cdec3ab8
kongxiangwen <xw.kong@hnair.com>
2019-05-23 13:49:00 +0800

    Add tdd module.

---
+++ dubbo-client/src/test/java/com/dcits/brave/aops/LocalMethodTracingTest.java
@@ -0,0 +1,36 @@
+package com.dcits.brave.aops;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocalMethodTracingTest {
+
+	@Test
+	void myMethod() {
+	}
+
+	@Test
+	void before() {
+	}
+
+	@Test
+	void after() {
+	}
+
+	@Test
+	void afterReturning() {
+	}
+
+	@Test
+	void afterThrowing() {
+	}
+
+	@Test
+	void around() {
+	}
+
+	@Test
+	void setApplicationContext() {
+	}
+}
---
+++ dubbo-client/src/test/java/com/dcits/brave/filters/BraveDubboFilterTest.java
@@ -0,0 +1,32 @@
+package com.dcits.brave.filters;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BraveDubboFilterTest {
+
+	@Test
+	void getObjectJsonStr() {
+	}
+
+	@Test
+	void getObjectMap() {
+	}
+
+	@Test
+	void getObjectMapStr() {
+	}
+
+	@Test
+	void invoke() {
+	}
+
+	@Test
+	void parseRemoteAddress() {
+	}
+
+	@Test
+	void onError() {
+	}
+}
